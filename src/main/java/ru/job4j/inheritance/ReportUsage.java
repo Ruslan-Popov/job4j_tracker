f2af8c2bff30e0ab4e25003d32ca44edb752154d
Ruslan-Popov <ruslan_popov@mail.ru>
2020-10-24 21:09:14 +0300

    1.2.2.5. Аннотация @Override [#288483]

--- src/main/java/ru/job4j/inheritance/ReportUsage.java
+++ src/main/java/ru/job4j/inheritance/ReportUsage.java
@@ -4,5 +4,7 @@ public class ReportUsage {
     public static void main(String[] args) {
         JSONReport j = new JSONReport();
         System.out.println(j.generate("Report's name", "Report's body"));
+        HtmlReport h = new HtmlReport();
+        System.out.println(h.generate("Report's name", "Report's body"));
     }
 }
