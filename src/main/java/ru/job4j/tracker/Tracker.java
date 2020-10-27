ee55664625db0cf25d0b60369abb220126dce2e4
Ruslan-Popov <ruslan_popov@mail.ru>
2020-10-27 23:30:55 +0300

    1.2.3.6. Метод замены заявки. Tracker.replace [#288491]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -37,15 +37,30 @@ public class Tracker {
         return Arrays.copyOf(itemsFoundByName, size);
     }
 
-    public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int i = 0; i < size; i++) {
+            if (items[i].getId() == id) {
+                rsl = i;
                 break;
             }
         }
         return rsl;
+    }
+
+    public Item findById(int id) {
+        int index = indexOf(id);
+        return index != -1? items[index] : null;
+    }
+
+    public boolean replace(int id, Item item) {
+        boolean rst = false;
+        int index = indexOf(id);
+        if (index != -1) {
+            item.setId(items[index].getId());
+            items[index] = item;
+            rst = true;
+        }
+        return rst;
     }
 }
