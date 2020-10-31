35e21b49bc11179e0acb6e1df80f8dcdf9493ef8
Ruslan-Popov <ruslan_popov@mail.ru>
2020-10-31 21:49:37 +0300

    1.2.4.2. Scanner и чтение числа из консоли. [#288426]

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -7,18 +7,15 @@ public class Matches {
         Scanner input = new Scanner(System.in);
         int matches = 11;
         boolean p1moved = false;
-        boolean p2moved = false;
         while (matches > 0) {
-            if (p1moved == false || p2moved == true) {
+            if (!p1moved) {
                 System.out.println("Первый игрок берет от 1 до 3 спичек");
                 int move = Integer.valueOf(input.nextLine());
                 if (move >= 1 && move <= 3) {
                     matches -= move;
-                    p1moved = true;
-                    p2moved = false;
+                    p1moved = !p1moved;
                 } else {
                     System.out.println("от 1 до 3х!");
-                    p2moved = true;
                 }
                 if (matches <= 0) {
                     System.out.println("Разобраны все спички. Победил первый игрок!");
@@ -26,17 +23,14 @@ public class Matches {
                 }
                 System.out.println("Осталось " + matches + " спичек");
             }
-            if (p2moved == false || p1moved == true) {
+            if (p1moved) {
                 System.out.println("Второй игрок берет от 1 до 3 спичек");
-
                 int move2 = Integer.valueOf(input.nextLine());
                 if (move2 >= 1 && move2 <= 3) {
                     matches -= move2;
-                    p2moved = true;
-                    p1moved = false;
+                    p1moved = !p1moved;
                 } else {
                     System.out.println("От 1 до 3х!");
-                    p1moved = true;
                 }
                 if (matches <= 0) {
                     System.out.println("Разобраны все спички. Победил второй игрок!");
--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,11 +1,44 @@
-package ru.job4j.tracker;
-
-public class StartUI {
-    public static void main(String[] args) {
-        Tracker track = new Tracker();
-        Item tomato = new Item(1, "Tomato");
-        track.add(tomato);
-        Item first = track.findById(1);
-        System.out.println(first.getName());
-    }
-}
+//package ru.job4j.tracker;
+//
+//import java.util.Scanner;
+//
+//public class StartUI {
+//
+//    public void init(Scanner scanner, Tracker tracker) {
+//        boolean run = true;
+//        while (run) {
+//            this.showMenu();
+//            int select = Integer.valueOf(scanner.nextLine());
+//            switch (select) {
+//                case 0:
+//                    System.out.println("=== Create a new item ====");
+//                    System.out.println("Enter name: ");
+//                    String name = scanner.nextLine();
+//                    Item item = new Item(name);
+//                    tracker.add(item);
+//                case 1:
+//
+//                case 6: run = false;
+//                default:
+//                    System.out.println("Enter value from 0 to 6");
+//        }
+//    }
+//
+//    private void showMenu() {
+//        System.out.println("Menu");
+//        System.out.println("0. Add new item");
+//        System.out.println("1. Show all items");
+//        System.out.println("2. Edit item");
+//        System.out.println("3. Delete item");
+//        System.out.println("4. Find item by id");
+//        System.out.println("5. Find items by name");
+//        System.out.println("6. Exit program");
+//        System.out.println("Select:");
+//    }
+//
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        Tracker tracker = new Tracker();
+//        new StartUI().init(scanner, tracker);
+//    }
+//}
