157392d4060b5668a9cc6539974a2160067b4f91
Ruslan-Popov <ruslan_popov@mail.ru>
2020-11-11 01:42:50 +0300

    1.2.5.0.3. Тестирование исключений с junit [#288446]

--- src/main/java/ru/job4j/ex/Fact.java
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -5,7 +5,7 @@ public class Fact {
         System.out.println("Fact of -1 is " + new Fact().calc(-1));
     }
 
-    public int calc(int n) {
+    public static int calc(int n) {
         if (n < 0) {
             throw new IllegalArgumentException("Argument should be >= 0");
         }
---
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -0,0 +1,11 @@
+package ru.job4j.ex;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class FactTest {
+    @Test(expected = IllegalArgumentException.class)
+    public void whenNLessThanZero() {
+        Fact.calc(-2);
+    }
+}
