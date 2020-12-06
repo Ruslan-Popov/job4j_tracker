a6c29e4239d891b47abcda84c4e77741252334c9
Ruslan-Popov <ruslan_popov@mail.ru>
2020-12-07 01:13:25 +0300

    1.3.1.1. Телефонный справочник на базе ArrayList

---
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,33 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+
+public class PhoneDictionary {
+    private ArrayList<Person> persons = new ArrayList<>();
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    public ArrayList<Person> find(String key) {
+        ArrayList<Person> result = new ArrayList<>();
+        for (int i = 0; i < persons.size(); i++) {
+            if (persons.get(i).getName().contains(key)) {
+                result.add(persons.get(i));
+                break;
+            }
+            if (persons.get(i).getSurname().contains(key)) {
+                result.add(persons.get(i));
+                break;
+            }
+            if (persons.get(i).getPhone().contains(key)) {
+                result.add(persons.get(i));
+                break;
+            }
+            if (persons.get(i).getAddress().contains(key)) {
+                result.add(persons.get(i));
+                break;
+            }
+        }
+        return result;
+    }
+}
