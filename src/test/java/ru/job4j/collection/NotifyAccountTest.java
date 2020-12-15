f4f22bbbf4ac622feb4f24a74b74e25a0caf4c23
Ruslan-Popov <ruslan_popov@mail.ru>
2020-12-16 00:33:15 +0300

    1.3.2.3. Модели данных и HashSet. [#288419]

---
+++ src/test/java/ru/job4j/collection/NotifyAccountTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class NotifyAccountTest {
+    @Test
+    public void sent() {
+        List<Account> accounts = Arrays.asList(
+                new Account("123", "Petr Arsentev", "eDer3432f" ),
+                new Account("142", "Petr Arsentev", "000001" )
+        );
+        HashSet<Account> expect = new HashSet<>(
+                Arrays.asList(
+                        new Account("123", "Petr Arsentev", "eDer3432f" ),
+                        new Account("142", "Petr Arsentev", "000001" )
+                )
+        );
+        assertThat(NotifyAccount.sent(accounts), is(expect));
+    }
+
+    @Test
+    public void sentUnique() {
+        List<Account> accounts = Arrays.asList(
+                new Account("123", "Petr Arsentev", "eDer3432f" ),
+                new Account("142", "Petr Arsentev", "000001" ),
+                new Account("142", "Arsentev Petr", "111110" )
+        );
+        HashSet<Account> expect = new HashSet<>(
+                Arrays.asList(
+                        new Account("123", "Petr Arsentev", "eDer3432f" ),
+                        new Account("142", "Arsentev Petr", "111110" )
+                )
+        );
+        assertThat(NotifyAccount.sent(accounts), is(expect));
+    }
+}
