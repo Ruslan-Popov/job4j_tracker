34bd6b4790351aa1cc2744754a2d98b32d653133
Ruslan-Popov <ruslan_popov@mail.ru>
2020-12-17 00:21:11 +0300

    1.3.2.2. Идентичные тексты. [#288418]

--- src/main/java/ru/job4j/collection/UniqueText.java
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -14,6 +14,7 @@ public class UniqueText {
         for (String dupl : text) {
             if (!check.contains(dupl)) {
                 rsl = false;
+                break;
             }
         }
         return rsl;
