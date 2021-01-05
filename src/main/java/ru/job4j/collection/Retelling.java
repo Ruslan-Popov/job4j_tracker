a7e907667aad37a5df4a6380b2459e765fa17b1a
Ruslan-Popov <ruslan_popov@mail.ru>
2021-01-05 23:26:25 +0300

    1.3.6. Контрольные вопросы. [#288396]

--- src/main/java/ru/job4j/collection/Retelling.java
+++ src/main/java/ru/job4j/collection/Retelling.java
@@ -6,9 +6,14 @@ import java.util.HashSet;
 public class Retelling {
     public static boolean generateBy(String originText, String duplicateText) {
         boolean rsl = true;
+        String[] orig = originText.split(" |\\r?\\n|,|\\.|!|;");
         String[] dupl = duplicateText.split(" ");
+        HashSet<String> temp = new HashSet<>();
+        for (String origWord : orig) {
+            temp.add(origWord);
+        }
         for (String word: dupl) {
-            if (!originText.contains(word)) {
+            if (!temp.contains(word)) {
                 rsl = false;
                 break;
             }
