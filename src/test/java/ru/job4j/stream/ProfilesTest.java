d6c8c78f4a290201af4cba5eecb1a9195b0e8da6
Ruslan-Popov <ruslan_popov@mail.ru>
2021-01-28 22:52:57 +0300

    1.4.2.3. Уникальность элементов и сортировка. [#288270]

--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -12,16 +12,18 @@ import static org.junit.Assert.*;
 public class ProfilesTest {
     @Test
     public void whenMap() {
-        Address address1 = new Address("SPb", "Nevskiy", 1, 1);
-        Address address2 = new Address("SPb", "Liteyniy", 1, 1);
+        Address address1 = new Address("Bryansk", "Nevskiy", 1, 1);
+        Address address2 = new Address("Anapa", "Arbat", 1, 1);
+        Address address3 = new Address("Vologda", "Sadovaya", 1, 1);
         List<Profile> profiles = new ArrayList<>(
                 Arrays.asList(
                         new Profile(address1),
-                        new Profile(address2)
+                        new Profile(address2),
+                        new Profile(address3)
                 )
         );
         List<Address> rsl = Profiles.collect(profiles);
-        List<Address> expected = Arrays.asList(address1, address2);
+        List<Address> expected = Arrays.asList(address2, address1, address3);
         assertThat(rsl, is(expected));
     }
 }
