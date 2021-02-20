e6f716bf466cfa76145ae1c520091db862b2c7f7
Ruslan-Popov <ruslan_popov@mail.ru>
2021-02-21 00:19:46 +0300

    1.4.3.4. Optional в Stream API. [#288281]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -18,16 +18,6 @@ public class BankService {
         }
     }
 
-//    public void addAccount(String passport, Account account) {
-//        User user = findByPassport(passport);
-//        Optional<User> rsl = Optional.of(user);
-//        if (rsl.isPresent()) {
-//            if (!users.get(user).contains(account)) {
-//                users.get(user).add(account);
-//            }
-//        }
-//    }
-
     public Optional<User> findByPassport(String passport) {
         return users.keySet()
                 .stream()
@@ -35,49 +25,16 @@ public class BankService {
                 .findFirst();
     }
 
-//    public User findByPassport(String passport) {
-//        return users.keySet()
-//                .stream()
-//                .filter(user -> user.getPassport().equals(passport))
-//                .findFirst()
-//                .orElse(null);
-//    }
-
     public Optional<Account> findByRequisite(String passport, String requisite) {
         Optional<User> rst = this.findByPassport(passport);
         if (rst.isPresent()) {
-            for (Account acc : users.get(rst.get())) {
-                if (acc.getRequisite().equals(requisite)) {
-                    return Optional.of(acc);
-                }
-            }
+            return users.get(rst.get())
+                    .stream()
+                    .filter(account -> account.getRequisite().equals(requisite))
+                    .findFirst();
         }
         return Optional.empty();
     }
-
-//    public Account findByRequisite(String passport, String requisite) {
-//        User user = this.findByPassport(passport);
-//        if (user != null) {
-//            for (Account acc : users.get(user)) {
-//                if (acc.getRequisite().equals(requisite)) {
-//                    return acc;
-//                }
-//            }
-//        }
-//        return null;
-//    }
-
-//    public Account findByRequisite(String passport, String requisite) {
-//        User user = findByPassport(passport);
-//        if (user != null) {
-//            return users.get(user)
-//                    .stream()
-//                    .filter(account -> account.getRequisite().equals(requisite))
-//                    .findFirst()
-//                    .orElse(null);
-//        }
-//        return null;
-//    }
 
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
